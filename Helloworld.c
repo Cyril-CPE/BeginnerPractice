5a5aa6cf8bf93663d03c749b0c4fe15ffdaa0502
Cyril-CPE <nalelouie15@gmail.com>
2025-04-17 20:34:17 +0800

    Room Temperature Logger

--- Helloworld.c
+++ Helloworld.c
@@ -1,30 +1,34 @@
 #include<stdio.h>
+//This program is a room temperature logger
+//teach me please :(
+int main (){
+    char room[20];
+    float reading1;
+    float reading2;
+    float reading3;
+    const float ideal = 27.3;
 
-int main(){
-    const float recdaily = 2.7;
-    float day1;
-    float day2;
-    float day3;
-    char name[10];
 
-    printf("-----Hydration Tracker----\n");
-    
-    printf("How many Liters for Day 1: \n");
-    scanf("%f",&day1);
+    printf("Location: ");
+    fgets(room, 20, stdin);
+    printf("Recording Temperature at %s \n",room);
 
-    printf("How many Liters for Day 2: \n");
-    scanf("%f",&day2);
+    printf("Enter 1st Reading: ");
+    scanf("%f",&reading1);
 
-    printf("How many Liters for Day 3: \n");
-    scanf("%f",&day3);
+    printf("Enter 2nd Reading: ");
+    scanf("%f",&reading2);
 
-    float totalWaterintake = day1 + day2 + day3;
-    float averageWaterintake = totalWaterintake / 3;
+    printf("Enter 3rd Reading: ");
+    scanf("%f",&reading3);
 
-    printf("You drank a total of %.3f Liters of Water in 3 days.\n",totalWaterintake);
-    printf("You drank an average %.2f Liter everyday\n", averageWaterintake);
+    printf("The following are the different readings:\n First Reading:%.2fC\tSecond Reading:%.2fC\tThird Reading:%.2fC", reading1 , reading2, reading3);
+    float totaltemp = reading1 + reading2 + reading3;
+    float averagetemp = totaltemp / 3;
+    float diff = ideal - averagetemp;
 
-    float intake = recdaily - averageWaterintake;
-    printf("%f", intake);
+    printf("\nAverage Temperature at %s is %.2fC. Enjoy your Stay\n", room, averagetemp);
+    printf("Difference from ideal temp is: %.2fC", diff);
+
     return 0;
 }
